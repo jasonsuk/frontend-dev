fc21e717dddd389099783bcd917b6cea200e62b7
Junghoon_Suk <sukjh87@gmail.com>
2020-10-08 11:39:08 +0800

    Mini-project Exchange Rate Calculator - HTML, CSS, JS : self-coded with a little guidance

---
+++ mini-projects/xrate-calculator/script.js
@@ -0,0 +1,38 @@
+const currencyOne = document.getElementById('currency-one');
+const currencyTwo = document.getElementById('currency-two');
+const amountOne = document.getElementById('amount-one');
+const amountTwo = document.getElementById('amount-two');
+const swapBtn = document.getElementById('swap');
+const rateText = document.getElementById('rate');
+
+const apiKey = '37a76d35abc5eaac2216a383';
+const baseUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+function getExchangeRate() {
+	const currency_one = currencyOne.value;
+	const currency_two = currencyTwo.value;
+	const url = baseUrl + currency_one;
+	// console.log(url);
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			const rate = data.conversion_rates[currency_two];
+			rateText.innerHTML = `1 ${currency_one} = ${rate} ${currency_two}`;
+			amountTwo.value = (rate * amountOne.value).toFixed(2);
+		}).catch(error => rateText.innerHTML = 'Sorry the exchange rate does not exist...');
+}
+
+amountOne.addEventListener('input', getExchangeRate);
+amountTwo.addEventListener('input', getExchangeRate);
+currencyOne.addEventListener('change', getExchangeRate);
+currencyTwo.addEventListener('change', getExchangeRate);
+
+swapBtn.addEventListener('click', (e) => {
+	const temp = currencyOne.value;
+	currencyOne.value = currencyTwo.value;
+	currencyTwo.value = temp;
+	getExchangeRate();
+})
+
+getExchangeRate();
