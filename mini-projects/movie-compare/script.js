69342a7394c33c66fa2a2454d390d4f01d348e24
Junghoon_Suk <sukjh87@gmail.com>
2020-10-24 21:11:54 +0800

    Mini-project Movie Compare - Fetched data on nav dropdown using Bulma CSS

--- mini-projects/movie-compare/script.js
+++ mini-projects/movie-compare/script.js
@@ -8,19 +8,67 @@ const baseUrl = 'http://www.omdbapi.com/';
 // 3. uses click on a movie of interest -
 // 4. fetch details of the movie of interest (by id)
 
-const searchMovie = async (searchInput) => {
+const autocomplete = document.getElementById('autocomplete');
+autocomplete.innerHTML = `
+    <div class="dropdown" id="dropdown">
+        <input type="text" id="input">
+        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+            <div class="dropdown-content" id="contents"></div>
+        </div>
+    </div>
+`
+
+const searchMovie = async (input) => {
+
     const response = await axios.get(baseUrl, {
         params : {
             apikey : API_KEY,
-            s: searchInput
+            s: input
         }
     });
 
-    console.log(response);
+    return response.data.Search;
 }
+    
+const input = document.querySelector('#input');
+const onInput =  ( async (e) => {
+    
+    const movies = await searchMovie(e.target.value);
 
+    if(!movies) {
+        dropdown.classList.remove('is-active')
+        return;
+    }
+    
+    contents.innerHTML = '';    
 
-const input = document.querySelector('input');
-const onInput = (e => searchMovie(e.target.movie));
+    movies.forEach(movie => {
+
+        const item = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? ' ' : movie.Poster;
+
+        item.classList.add('dropdown-item');
+
+        item.innerHTML = `
+        <img src=${imgSrc}>
+        <span>${movie.Title}</span>
+        `;
+
+        const contents = document.getElementById('contents');
+        const dropdown = document.getElementById('dropdown');
+
+        contents.appendChild(item);
+        dropdown.classList.add('is-active');
+    });
+    
+});
+
 input.addEventListener('input', debounce(onInput, 500));
 
+// When click on any blank window area, the dropdown disappers
+document.addEventListener('click', (e) => {
+    // console.log(e.target);
+    if(!autocomplete.contains(e.target)) {
+        document.getElementById('dropdown').classList.remove('is-active');
+    }
+})
--- mini-projects/movie-compare/utils.js
+++ mini-projects/movie-compare/utils.js
@@ -2,10 +2,10 @@
 // Refactorted with debounce function 
 const debounce = (func, delay=1000) => {
     let timeOutId;
-    return (...arg) => {
+    return (...args) => {
         if(timeOutId) clearTimeout(timeOutId);
         timeOutId = setTimeout(() => {
-            func.apply(null, ...arg);
+            func.apply(null, args);
         }, delay);
     }
 }
