8b3530f4638c8c5067a18d26ca87c727963fb803
Junghoon_Suk <sukjh87@gmail.com>
2020-11-02 10:08:57 +0800

    Mini-project Speech Text Reader - Completed with Speech API (speechSynthesis)

--- mini-projects/speech-text-reader/script.js
+++ mini-projects/speech-text-reader/script.js
@@ -49,6 +49,7 @@ const data = [
     },
 ];
 
+// Create image box for <main>
 data.forEach(createBox);
 
 function createBox(item) {
@@ -60,5 +61,82 @@ function createBox(item) {
         <img src=${image} alt=${text}>
         <p>${text}</p>
     `;
+
+    box.addEventListener('click', () => {
+        setTextMessage(text);
+        speakVoiceText();
+
+        box.classList.add('effect');
+        setTimeout(() => {
+            box.classList.remove('effect');
+        }, 800);
+    });
+
     document.querySelector('main').appendChild(box);
 }
+
+const textBox = document.getElementById('textBox');
+const toggleBtn = document.getElementById('toggleBtn');
+const closeBtn = document.getElementById('closeBtn');
+const voiceSelect = document.getElementById('voiceSelect');
+const text = document.getElementById('text');
+const readBtn = document.getElementById('readBtn');
+
+let voices = [];
+
+// Speech API
+const populateVoices = () => {
+    if (typeof speechSynthesis === 'undefined') {
+        console.log('Speech Synthesis API undefined...');
+        return;
+    }
+
+    voices = speechSynthesis.getVoices();
+    voices.forEach((voice) => {
+        const option = document.createElement('option');
+
+        option.value = voice.name;
+        option.innerText = `${voice.name} (${voice.lang})`;
+
+        if (voice.default) {
+            option.innerText += ' -- default';
+        }
+
+        voiceSelect.appendChild(option);
+    });
+};
+
+const utterThis = new SpeechSynthesisUtterance();
+const setTextMessage = (text) => (utterThis.text = text);
+const speakVoiceText = () => speechSynthesis.speak(utterThis);
+
+const setVoice = (e) => {
+    utterThis.voice = voices.find((voice) => voice.name === e.target.value);
+};
+
+// Toggle text box
+const showTextBox = () => textBox.classList.add('show');
+const closeTextBox = () => textBox.classList.remove('show');
+
+toggleBtn.addEventListener('click', showTextBox);
+closeBtn.addEventListener('click', closeTextBox);
+
+// Speech API event listner
+populateVoices();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.addEventListener('voiceschanged', populateVoices);
+}
+
+// When a different voice is selected
+voiceSelect.addEventListener('change', setVoice);
+
+readBtn.addEventListener('click', (e) => {
+    if (text.value) {
+        setTextMessage(text.value);
+        speakVoiceText();
+    } else {
+        const message = 'Please enter your text...!';
+        setTextMessage(message);
+        speakVoiceText();
+    }
+});
