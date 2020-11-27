694c29ce7a6ff82c0809921a779e2515aeb08a99
Junghoon_Suk <sukjh87@gmail.com>
2020-11-27 10:34:42 +0800

    React: E-comm - Broke down into smaller components and styled them

--- react/main/ecomm-clothing/src/App.js
+++ react/main/ecomm-clothing/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage.component';
 
 function App() {
     return (
---
+++ react/main/ecomm-clothing/src/pages/homepage.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Directory from '../components/directory/directory.component';
+
+// Load sass styles
+import './homepage.styles.scss';
+
+// When the state or lifecyle methods don't need being specified
+// functional component (as opposed to class component) would just do!
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <Directory className="directory-menu" />
+        </div>
+    );
+};
+
+export default Homepage;
