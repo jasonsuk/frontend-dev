dd629833def8b54905559d99a61ccf261df96882
Junghoon_Suk <sukjh87@gmail.com>
2020-12-11 10:44:11 +0800

    Applied Redux-presist module to save cart items on reload

--- react/main/ecomm-clothing/src/redux/root-reducer.js
+++ react/main/ecomm-clothing/src/redux/root-reducer.js
@@ -1,11 +1,22 @@
 // @DESC: Represents the overall reducers
 
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // storage path
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart'],
+    // only cart will be persisted (user @ Firebase)
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- react/main/ecomm-clothing/src/redux/store.js
+++ react/main/ecomm-clothing/src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger'; // use when debugging redux code
+import { persistStore } from 'redux-persist'; // localstorage under the hood
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger]; // array of middlewares
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store; // passed to index.js as a prop of <Provider />
+export const persistor = persistStore(store);
+
+// export default { store, persistor }; // passed to index.js as a prop of <Provider />
